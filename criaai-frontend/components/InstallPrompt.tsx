f4d8fd9cff120838b7266f5983b6f152537bd20f
brenojunior79-mcpia <brenojunior79@gmail.com>
2026-09-26 13:21:22 -0300

    Update InstallPrompt.tsx

--- criaai-frontend/components/InstallPrompt.tsx
+++ criaai-frontend/components/InstallPrompt.tsx
@@ -24,7 +24,7 @@ export default function InstallPrompt() {
         return
       }
 
-      function handleBeforeInstall(e: any) {
+      const handleBeforeInstall = function(e: any) {
         e.preventDefault()
         setDeferredPrompt(e)
         setShowBanner(true)
